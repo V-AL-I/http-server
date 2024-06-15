a6f9580e839e91a1051ed388c98014c0d6109b6d
VALI <thomas.solatges@gmail.com>
2024-06-15 14:13:04 +0800

    fast commit mac

--- app/server.c
+++ app/server.c
@@ -7,6 +7,26 @@
 #include <errno.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
+int get_index_of_substring(char* source, char* substring) {
+    // return the index of the first substring if found,
+    // else return -1
+    int srcLen = strlen(source);
+    int subStrLen = strlen(substring);
+    char* sourceCopy = calloc(srcLen, sizeof(char));
+    strcpy(sourceCopy, source);
+    int count = 0;
+    while (sourceCopy[0] != '\0'){
+        if (strncmp(sourceCopy, substring, subStrLen) == 0) return count;
+        else {
+            count++;
+            sourceCopy++;
+        }
+    }
+    return -1;
+}
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
@@ -14,6 +34,7 @@ int main() {
 
 	int server_fd, client_addr_len;
 	struct sockaddr_in client_addr;
+    char buffer[BUFFER_SIZE] = {0};
 	
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
@@ -49,9 +70,24 @@ int main() {
 	client_addr_len = sizeof(client_addr);
 	
 	int client = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-    char* reply = "HTTP/1.1 200 OK\r\n\r\n";
-    send(client, reply, strlen(reply), 0);
-	printf("Client connected\n");
+    if (read(client, buffer, BUFFER_SIZE) < 0) perror("read");
+    
+        printf("Client connected\n");
+
+    int command = get_index_of_substring(buffer, "GET");
+    if (command >= 0) {
+        if (buffer[4] == '/' && buffer[5] == ' ') {
+            char* reply = "HTTP/1.1 200 OK\r\n\r\n";
+            send(client, reply, strlen(reply), 0);
+        }
+        else {
+            char* reply = "HTTP/1.1 404 Not Found\r\n\r\n";
+            send(client, reply, strlen(reply), 0);
+        }
+    }
+
+    //printf("buffer = %s\n", buffer);
+
 	
 	close(server_fd);
 
